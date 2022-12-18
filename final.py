116a9eff8f675a0933be1f924857ccba54717bd3
AbelMendoza-fsociety <120528976+AbelMendoza-fsociety@users.noreply.github.com>
2022-12-18 18:56:11 -0500

    Update final.py

--- final.py
+++ final.py
@@ -1,15 +1,11 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
 
 import streamlit as st
 
 
-st.write(*<center> <h1>Universidad Nacional de San Agustín de Arequipa</h1> </center>*) 
+st.litle("Unsa")
+
  
-st.write(*<center> <h1>Escuela Profesional de Ingeniería de Telecomunicaciones</h1> </center>*) 
+st.write (*<center> <h1>Escuela Profesional de Ingeniería de Telecomunicaciones</h1> </center>*) 
 # 
 # st.write(*<center> <h1> </h1> </center>*) 
 # 
